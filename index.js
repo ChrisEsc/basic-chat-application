65a241c1d52a65c1dd30fa07c66d678d3ef03a23
Christian Esclamado <chrisesclamado@gmail.com>
2022-07-28 15:02:49 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *: 3000');
+});
+
+// https://socket.io/get-started/chat
+// homework
+// Here are some ideas to improve the application:
+
+// Broadcast a message to connected users when someone connects or disconnects.
+// Add support for nicknames.
+// Don’t send the same message to the user that sent it. Instead, append the message directly as soon as he/she presses enter.
+// Add “{user} is typing” functionality.
+// Show who’s online.
+// Add private messaging.
+// Share your improvements!
